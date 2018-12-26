03b608d2d623b3f4fe552a47d05419b47c39be8c
mostafiz9900 <mostafiz9900@gmail.com>
2018-12-26 13:56:28 +0600

    just start

---
+++ meetingcall/src/main/java/com/beskilled/onlinebanking/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.beskilled.onlinebanking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping(value = "/")
+    public String displayHome(){
+        return "layout";
+    }
+    @GetMapping(value = "/fo.jsf")
+    public String displayFooter(){
+        return "fragment/nav";
+    }
+
+
+}
